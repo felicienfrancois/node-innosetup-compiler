16b78d60368739e6f89ac3c235de53876a8b3548
felicienfrancois <felicien@tweakstyle.com>
2014-08-01 12:04:44 +0200

    fix

--- tasks/grunt-iscc.js
+++ tasks/grunt-iscc.js
@@ -2,7 +2,7 @@
 module.exports = function(grunt) {
 
 	grunt.registerMultiTask('innosetup-compiler', 'Node wrapper to compile inno setup scripts (.iss)', function() {
-		var iscc = require("../lib/innosetup-compiler.js");
+		var iscc = require("../lib/iscc.js");
 		var done = this.async();
 		iscc(this.data.script, {}, function(error) {
 			done(!error);
