8ba0a9e77d450db24406cdc47d86cb2e506ee0cd
felicienfrancois <felicien@tweakstyle.com>
2014-08-01 11:53:23 +0200

    path fixes

--- tasks/grunt-iscc.js
+++ tasks/grunt-iscc.js
@@ -2,7 +2,7 @@
 module.exports = function(grunt) {
 
 	grunt.registerMultiTask('innosetup-compiler', 'Inno setup compiler', function() {
-		require("../lib/innosetup-compiler.js");
+		require("../lib/iscc.js");
 		var done = this.async();
 		exec(this.data.script, this.data.options, function(error) {
 			done(!error);
